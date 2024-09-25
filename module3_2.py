d4c30a565c03db2fafe88090e871158ffb32596b
Диана <103692293+LitleDiana@users.noreply.github.com>
2024-09-25 23:09:09 +0300

    Update module3_2.py

--- module3_2.py
+++ module3_2.py
@@ -5,10 +5,10 @@ def send_email(message,recipient,sender = "university.help@gmail.com"):
     recipient.endswith(".ru") or recipient.endswith(".net"))):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
 
-    if sender==recipient:
+    elif sender==recipient:
         print("Нельзя отправить письмо самому себе!")
 
-    if sender== "university.help@gmail.com":
+    elif sender== "university.help@gmail.com":
         print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
     else:
         print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
